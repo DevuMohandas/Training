03f65baae71dc51ca949b7eedd3925d20787408d
Devu Mohandas <devumohandas2019@gmail.com>
2025-02-19 08:52:04 +0530

    Updated Avatar component

--- src/components/atoms/Avatar.tsx
+++ src/components/atoms/Avatar.tsx
@@ -20,8 +20,7 @@ const Avatar: React.FC<AvatarProps> = ({ name, image }) => {
 
   return (
     <div className="flex justify-center items-center w-[2.875rem] h-[2.875rem] rounded-full bg-gradient-to-r from-[#61A6F2] via-[#C9649A] to-[#F28E4C] overflow-hidden">
-      {name && <div className="text-[#FFFFFF] font-[700] text-[1rem]">{initials && initials.toUpperCase()}</div>}
-      {image && <Image alt="Profile" src={image} width={100} height={100} className="w-[2rem] h-[2rem] rounded-full" />}
+      {image ? <Image alt="Profile" src={image} width={100} height={100} className="w-[2rem] h-[2rem] rounded-full" /> : <div className="text-[#FFFFFF] font-[700] text-[1rem]">{initials && initials.toUpperCase()}</div>}
     </div>
   );
 };
